a731902963b549aadf65f8d0873fff171e1bc936
Jakob Dayton <jakobdayton@gmail.com>
2022-11-21 19:32:38 -0500

    linked CocktailDB API

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,23 +1,35 @@
 // var requestMDBapi =
 // var requestMotNapi =
-
-var movieTitleEl = $("#movie-title");
-var moviePosterEl = $("#movie-poster");
-var movieDescriptionEl = $("#movie-description");
-var movieYearEl = $("#movie-year");
-var movieRatingEl = $("#movie-rating");
-var iconNetflixEl = $("#icon-netflix");
-var iconHuluEl = $("#icon-hulu");
-var iconPrimeEl = $("#icon-prime");
-var iconHBOEl = $("#icon-HBO");
+var submitEl = document.querySelector("#submit");
+var movieEl = document.querySelector("#movie");
+var movieTitleEl = document.querySelector("#movie-title");
+var moviePosterEl = document.querySelector("#movie-poster");
+var movieDescriptionEl = document.querySelector("#movie-description");
+var movieYearEl = document.querySelector("#movie-year");
+var movieRatingEl = document.querySelector("#movie-rating");
+var iconNetflixEl = document.querySelector("#icon-netflix");
+var iconHuluEl = document.querySelector("#icon-hulu");
+var iconPrimeEl = document.querySelector("#icon-prime");
+var iconHBOEl = document.querySelector("#icon-HBO");
 // var streamingServices = {
 
 // }
 
 
-var submitEl = $("#submit");
 
-submitEl.click(function(){
+
+
+
+submitEl.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log("u working bb?");
+    var movie = movieEl.value; 
+    getMovieAPI(movie);
+    getCocktailDB();
+})
+
+
+function getMovieAPI(movie){
 /*populate movie variables with api data
 
 
@@ -43,6 +55,20 @@ for every streaming service
         hide HBO icon
     }
    */ 
+}
 
-})
 
+function getCocktailDB(){
+    var queryUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
+
+    fetch(queryUrl)
+        .then(function (response){
+            console.log("working?");
+            return response.json();
+        })
+        .then(function (cocktailData){
+            console.log(cocktailData);
+        })
+
+
+}
